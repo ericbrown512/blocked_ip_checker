730a9ea0a853861ed01905266fb9b5090feed452
Eric Brown <ericalexanderbrown91@gmail.com>
2021-08-04 14:54:11 -0500

    changed port

--- dist/app.js
+++ dist/app.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
     let ip = req.query.ip;
     res.send(ip);
