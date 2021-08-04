13f9dc74486acaf68cc69aafe66b27e005e69979
Eric Brown <ericalexanderbrown91@gmail.com>
2021-08-04 16:03:55 -0500

    Added code to check db

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,27 @@ import express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+var mysql      = require('mysql');
+var pool  = mysql.createPool({
+    connectionLimit : 10,
+    host            : 'db-blocked-ips.cnws0qrfgqmq.us-east-2.rds.amazonaws.com',
+    user            : 'admin',
+    password        : 'password123',
+    database        : 'sys'
+});
+
 app.get("/", (req, res) => {
 
     let ip = req.query.ip;
-    res.send(ip);
 
+    pool.query('SELECT id FROM sys.blocked_ips WHERE `ip` = ?', ip, function (error, results, fields) {
+        if (error) throw error;
+        if(results.length > 0) {
+            res.send(1);
+        } else {
+            res.send(0);
+        }
+    });
 });
 
 app.listen(port, () => {
