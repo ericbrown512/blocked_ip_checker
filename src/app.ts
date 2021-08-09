faebc0cdd9435076eb8a12a5e90e680f1645408f
Eric Brown <ericalexanderbrown91@gmail.com>
2021-08-09 16:53:37 -0500

    Removed some debug code

--- src/app.ts
+++ src/app.ts
@@ -9,12 +9,16 @@ app.get("/", async (req, res) => {
 
     let ip = req.query.ip;
     try {
-        var ipInt = ipInt(ip).toInt();
-        var result = await db.query('SELECT count(id) FROM blocked_ips WHERE ? >= ip_range_start AND ? <= ip_range_end', [ipInt, ipInt]);
+        //Convert ip address to an integer
+        var ipNum = ipInt(ip).toInt();
+
+        //Check if the integer is in any ip ranges we have listed
+        var result = await db.query('SELECT id FROM blocked_ips WHERE ? >= ip_range_start AND ? <= ip_range_end', [ipNum, ipNum]);
         if(result.length > 0) {
             res.json({success: true, found: true});
+        } else {
+            res.json({success: true, found: false});
         }
-        res.json({success: true, found: false});
     } catch (err) {
         res.json({success: false, found: false, error: err.message});
         console.error(`Error while checking ip `, err.message);
